6cce00dd65ef580d195ddef3fc0c7f98c691bb31
Chirs Greene <chigreenwork@gmail.com>
2023-10-21 09:37:46 -0400

    adds columb definitions for tag

--- develop/models/ProductTag.js
+++ develop/models/ProductTag.js
@@ -7,15 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
-    id: {
-      type: DataType.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    tag_name: {
-      type: DataType.STRING
+   id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+   },
+   production_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'product',
+      key: 'id'
     }
+   },
+   tag_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'tag',
+      key: 'id'
+    }
+   }
   },
   {
     sequelize,
